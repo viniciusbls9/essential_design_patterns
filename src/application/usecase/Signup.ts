bb8ecbeda5a2955cb7f9d77a51cbdb524666761b
Vinicius <beneditoloura@gmail.com>
2023-05-29 07:45:19 -0300

    refactor: remove signup validation to User domain

--- src/application/usecase/Signup.ts
+++ src/application/usecase/Signup.ts
@@ -5,24 +5,6 @@ export default class Signup {
   constructor(readonly userRepository: UserRepository) {}
 
   async execute(input: Input): Promise<void> {
-    if (input.name.split(' ').length < 2) {
-      throw new Error('Invalid name')
-    }
-
-    if(!String(input.email).toLowerCase().match(/^[\w-]+(\.[\w-]+)*@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/)) {
-			throw new Error("Invalid email");
-		}
-
-    if (input.password.length < 8) {
-      throw new Error("Invalid password");
-      
-    }
-
-    if (input.age < 18) {
-      throw new Error("Invalid age");
-      
-    }
-
     const user = new User(input.name, input.email, input.password, input.age)
     await this.userRepository.save(user)
   }
