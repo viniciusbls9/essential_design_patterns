69fc98b6d3856bc97c29fa289313e9e1ecdbb716
Vinicius <beneditoloura@gmail.com>
2023-05-29 07:45:40 -0300

    refactor: validation info to make signup

--- src/domain/entity/User.ts
+++ src/domain/entity/User.ts
@@ -1,3 +1,19 @@
 export default class User {
-  constructor (readonly name: string, readonly email: string, readonly password: string, readonly age: number) {}
+  constructor (readonly name: string, readonly email: string, readonly password: string, readonly age: number) {
+    if (name.split(' ').length < 2) {
+      throw new Error('Invalid name')
+    }
+
+    if(!String(email).toLowerCase().match(/^[\w-]+(\.[\w-]+)*@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/)) {
+			throw new Error("Invalid email");
+		}
+
+    if (password.length < 8) {
+      throw new Error("Invalid password");
+    }
+
+    if (age < 18) {
+      throw new Error("Invalid age");
+    }
+  }
 }
